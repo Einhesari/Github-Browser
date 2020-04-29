45aa2b7122964c2b882f560161a9c21b071dbb35
Einhesari <mohseneinhesari@yahoo.com>
2020-04-29 13:48:54 +0430

    add auth activity

---
+++ app/src/main/java/alfarobot/gss/com/githubbrowser/presentation/AuthActivity.kt
@@ -0,0 +1,13 @@
+package alfarobot.gss.com.githubbrowser.presentation
+
+import alfarobot.gss.com.githubbrowser.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}

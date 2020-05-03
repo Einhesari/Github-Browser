97a42599b8b61dca6782c4e3dbb08456923ced97
Einhesari <mohseneinhesari@yahoo.com>
2020-05-03 14:02:24 +0430

    1/2 implementing webview fragment

--- app/src/main/java/alfarobot/gss/com/githubbrowser/presentation/auth/GithubWebViewFragment.kt
+++ app/src/main/java/alfarobot/gss/com/githubbrowser/presentation/auth/GithubWebViewFragment.kt
@@ -14,6 +14,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_github_webview.*
 
 class GithubWebViewFragment : Fragment() {
 
@@ -49,13 +50,14 @@ class GithubWebViewFragment : Fragment() {
                     return false
                 }
             } ?: run {
+                webviewLoading.show()
                 return super.shouldOverrideUrlLoading(view, request)
             }
         }
 
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
-            //TODO Hide Loading
+            webviewLoading.hide()
         }
 
         override fun onReceivedError(
@@ -64,6 +66,7 @@ class GithubWebViewFragment : Fragment() {
             error: WebResourceError?
         ) {
             super.onReceivedError(view, request, error)
+            webviewLoading.hide()
             //TODO GO BACK
         }
     }
